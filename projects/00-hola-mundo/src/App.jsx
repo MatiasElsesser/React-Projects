c4e9ac37241a78d1b2e29bd193967cc9c1811cb9
ElsesserMatias <elsesser3@gmail.com>
2023-01-21 19:32:29 -0300

    Continuacion tarjetas de follow twitter

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,14 +1,28 @@
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
+      const format = (userName) => `@ ${userName}`
+
       return (
                   <section className='App'>
                         <TwitterFollowCard userName="matielsesser"
                         name="Matias Elsesser"
-                        />
+                        formatUsername={format}
+                        // pasamos la funcion sola, porque si pasamos la ejecucion de la funcion "formatUsername()" nos devolveria lo que devuelve la funcion
+                        >
+                              Mati Elsesser
+                        </TwitterFollowCard>
                         <TwitterFollowCard userName="elonmusk"
-                        name="Elon Musk"
-                        />
+                        //podemos pasar el parametro solo y al ser un boleano lo tomaria como true
+                        formatUsername={format}>
+                              Elon Musk 
+                        </TwitterFollowCard>
+                        <TwitterFollowCard 
+                        userName="pepito"
+                        // name="El Pepito"
+                        formatUsername={format}>
+                              El Pepito
+                        </TwitterFollowCard>
                   </section>
       )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,17 +1,45 @@
 import './TwitterFollowCard.css'
-export function TwitterFollowCard ({userName, name, isFollowing}) {
+import { useState } from 'react' //importamos el Hook
+
+
+export function TwitterFollowCard ({ formatUsername ,userName, children}) {
+      // tambien podemos pasar funciones como props (formatUsername)
+
+
+      const [ isFollowing, setIsFollowing] = useState(false)
+
+
+
+      //renderizado condicional: le decimos al boton de texto que queremos que diga dependiendo si la prop isFollowing es true o false
+      const text = isFollowing
+            ? "Siguiendo"
+            : "Seguir"
+      
+      const btnFollowClassName = isFollowing
+            ? "tw-followCard-btnFollow is-following"
+            : "tw-followCard-btnFollow"
+
+      const handleClick = () => {
+            setIsFollowing(!isFollowing)
+      }
+
+
       return (
             <article className='tw-followCard'>
                   <header className='tw-followCard-header'>
                         <img className='tw-followCard-image' alt="el avatar de microlink" src={`https://unavatar.io/${userName}`}/>
                         <div className='tw-followCard-nickContainer'>
-                              <strong> { name } </strong>
-                              <span className='tw-followCard-user'> @{ userName } </span>
+                              <strong>{ children } </strong>
+                              {/* children se refiere a todo lo que este dentro del elemento que se renderiza, tanto como un texto o un elemento, la prop children recibe eso */}
+                              <span className='tw-followCard-user'> {formatUsername(  userName) } </span>
                         </div>
                   </header>
 
                   <aside>
-                        <button className='tw-followCard-btnFollow'> Seguir </button>
+                        {/* aqui le damos el condicional a la clase para que sea dinamico y elija el estilo dependiendo el estado de la prop */}
+                        <button 
+                        className={btnFollowClassName}
+                        onClick={handleClick}> {text} </button>
                   </aside>
             </article>
       )
