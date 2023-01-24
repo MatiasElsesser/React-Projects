a257fb070cba8ad3f114e49b1c345d3ecc5a1aa3
ElsesserMatias <elsesser3@gmail.com>
2023-01-24 13:28:27 -0300

    inicio de la cuadricula del tateti

--- projects/02-ta-te-ti/src/App.jsx
+++ projects/02-ta-te-ti/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+const TURNS = {
+	X: "x",
+	O:"o"
+}
+
+const board = Array(9).fill(null) // fill() cambia los valores del array
+
+const Square = ( { children, updateBoard, index } ) => {
+	return (
+		<div className="square">
+			{children}
+		</div>
+	)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (  
+		<main className="board">
+			<h1>Ta te ti</h1>
+			<section className="game">
+				{
+					board.map( (_, index) => {
+						return (
+							<Square
+							key={index}
+							index={index}>
+							{index}
+							</Square>
+						)
+					} )
+				}
+			</section>
+		</main>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+)
 }
 
 export default App
