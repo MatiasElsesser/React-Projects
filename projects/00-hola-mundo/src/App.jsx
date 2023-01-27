9dd3712316ffaf6a83793b59dca5a7a190586780
ElsesserMatias <elsesser3@gmail.com>
2023-01-26 22:54:02 -0300

    Intall standardJS

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,88 +1,87 @@
 import { TwitterFollowCard } from './TwitterFollowCard'
-import {useState} from "react"
-
+import { useState } from 'react'
 
 const users = [
-      {
-            userName: 'matielsesser',
-            name: 'Matias Elsesser',
-            isFollowing: true
-      },
-      {
-            userName: 'GiuliElsesser',
-            name: 'Giuliana Elsesser',
-            isFollowing: false
-      },
-      {
-            userName: 'elonmusk',
-            name: 'Elon Musk',
-            isFollowing: false
-      },
-      {
-            userName: 'moniservi',
-            name: 'Monica Servi',
-            isFollowing: true
-      }
+  {
+    userName: 'matielsesser',
+    name: 'Matias Elsesser',
+    isFollowing: true
+  },
+  {
+    userName: 'GiuliElsesser',
+    name: 'Giuliana Elsesser',
+    isFollowing: false
+  },
+  {
+    userName: 'elonmusk',
+    name: 'Elon Musk',
+    isFollowing: false
+  },
+  {
+    userName: 'moniservi',
+    name: 'Monica Servi',
+    isFollowing: true
+  }
 ]
 
-
-
-
 export function App () {
-      const format = (userName) => `@ ${userName}`
+  const format = (userName) => `@ ${userName}`
 
-      const [name, setName] = useState("matielsesser")
-      console.log("render with:", name) // se ejecuta cada vez que cambia el estado
-      // cuando el elemento padre cambia su estado todos sus hijos se vuelven a renderizar en el virtual Dom
+  const [name, setName] = useState('matielsesser')
+  console.log('render with:', name) // se ejecuta cada vez que cambia el estado
+  // cuando el elemento padre cambia su estado todos sus hijos se vuelven a renderizar en el virtual Dom
 
-      return (
-                  <section className='App'>
-                        <TwitterFollowCard 
-                        initialIsFolowing
-                        userName={name}
-                        name="Matias Elsesser"
-                        formatUsername={format}
-                        // pasamos la funcion sola, porque si pasamos la ejecucion de la funcion "formatUsername()" nos devolveria lo que devuelve la funcion
-                        >
-                              Mati Elsesser
-                        </TwitterFollowCard>
+  return (
+    <section className='App'>
+      <TwitterFollowCard
+        initialIsFolowing
+        userName={name}
+        name='Matias Elsesser'
+        formatUsername={format}
+      >
+        Mati Elsesser
+      </TwitterFollowCard>
 
-                        <TwitterFollowCard userName="elonmusk"
-                        //podemos pasar el parametro solo y al ser un boleano lo tomaria como true
-                        initialIsFolowing
-                        formatUsername={format}>
-                              Elon Musk 
-                        </TwitterFollowCard>
+      <TwitterFollowCard
+        userName='elonmusk'
+                        // podemos pasar el parametro solo y al ser un boleano lo tomaria como true
+        initialIsFolowing
+        formatUsername={format}
+      >
+        Elon Musk
+      </TwitterFollowCard>
 
-                        <TwitterFollowCard 
-                        initialIsFolowing={false}
-                        userName="pepito"
+      <TwitterFollowCard
+        initialIsFolowing={false}
+        userName='pepito'
                         // name="El Pepito"
-                        formatUsername={format}>
-                              El Pepito
-                        </TwitterFollowCard>
+        formatUsername={format}
+      >
+        El Pepito
+      </TwitterFollowCard>
 
-                        {
-                              users.map( ( user ) => {
-                                    {/* desestructuramos  para obtener mas facil los valores de los objetos*/}
-                                    const { userName, name, isFollowing } = user
-                                    return (
-                                          <TwitterFollowCard 
+      {
+                              users.map((user) => {
+                                { /* desestructuramos  para obtener mas facil los valores de los objetos */ }
+                                const { userName, name, isFollowing } = user
+                                return (
+                                  <TwitterFollowCard
                                           // la Key es un identificador unico que nos va  a pedir al momento de renderizar listas, para asi poder identificar cada elemento por separado en el VirtualDom. Siempre asegurarse de darle un valor unico, que no se repita
-                                          key={userName}
-                                          userName={userName}
-                                          isFollowing={isFollowing}
-                                          formatUsername={format}>
-                                                {name}
-                                          </TwitterFollowCard>
-                                    )
+                                    key={userName}
+                                    userName={userName}
+                                    isFollowing={isFollowing}
+                                    formatUsername={format}
+                                  >
+                                    {name}
+                                  </TwitterFollowCard>
+                                )
                               })
                         }
-                        {/* De la manera anterior podemos renderizar listas de objetos, al fin y al cabo sigue siendo javascript */}
+      {/* De la manera anterior podemos renderizar listas de objetos, al fin y al cabo sigue siendo javascript */}
 
-                        <button onClick={ () => setName("midudev") }>
-                              Cambio de nombre
-                        </button>
-                  </section>
-      )
+      <button onClick={() => setName('midudev')}>
+        Cambio de nombre
+      </button>
+    </section>
+  )
 }
--- projects/02-ta-te-ti/src/App.jsx
+++ projects/02-ta-te-ti/src/App.jsx
@@ -1,95 +1,89 @@
-import { useState } from "react"
+import { useState } from 'react'
 import confetti from 'canvas-confetti'
-import { Square } from "./components/Square.jsx"
-import { TURNS } from "./constants.js"
-import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
-import { WinnerModal } from "./components/WinnerModal.jsx"
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
 
+function App () {
+  const [board, setBoard] = useState(() => {
+    // hacemos esto porque NUNCA se debe poner un estado dentro de un condicional
+    // ademas buscar en el local storage es lento y sincrono. Por ende si buscamos cada vez que renderiza le quitaria rendimiento a la aplicacion
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  }) // fill() cambia los valores del array
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    // "??" devuelve lo del lado derecho cuando lo del lado izquierdo es NULL o UNDEFINED
+    return turnFromStorage ?? TURNS.X
+  })
 
-function App() {
-	const [board, setBoard] = useState( () => {
-		// hacemos esto porque NUNCA se debe poner un estado dentro de un condicional
-		// ademas buscar en el local storage es lento y sincrono. Por ende si buscamos cada vez que renderiza le quitaria rendimiento a la aplicacion
-		const boardFromStorage = window.localStorage.getItem("board")
-		return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)}) // fill() cambia los valores del array
-	const [turn, setTurn] = useState(() => {
-		const turnFromStorage = window.localStorage.getItem("turn")
-		// "??" devuelve lo del lado derecho cuando lo del lado izquierdo es NULL o UNDEFINED
-		return turnFromStorage ?? TURNS.X})
-		
+  // null que no hay ganador y false empate
+  const [winner, setWinner] = useState(null)
 
-	//null que no hay ganador y false empate
-	const [winner, setWinner] = useState(null)
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
 
+    window.localStorage.clear()
+  }
 
+  const updateBoard = (index) => {
+    if (board[index] || winner) return // si ya tiene algo no acrualizamos la posicion
 
+    // actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
-	const resetGame = () => {
-		setBoard(Array(9).fill(null))
-		setTurn(TURNS.X)
-		setWinner(null)
+    // cambiar turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
 
-		window.localStorage.clear()
-	}
+    // guardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+    // revisar si hay ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) // empate
+    }
+  }
 
-
-	const updateBoard = ( index ) => {
-		if (board[index] || winner) return // si ya tiene algo no acrualizamos la posicion
-
-		//actualizar el tablero
-		const newBoard = [ ... board ]
-		newBoard[index] = turn
-		setBoard(newBoard)
-
-
-		//cambiar turno
-		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-		setTurn(newTurn)
-
-		// guardar partida
-		window.localStorage.setItem("board", JSON.stringify(newBoard))
-		window.localStorage.setItem("turn", newTurn)
-		//revisar si hay ganador
-		const newWinner = checkWinnerFrom(newBoard)
-		if ( newWinner ) {
-			confetti()
-			setWinner(newWinner)
-		} else if (checkEndGame(newBoard)) {
-			setWinner(false) // empate
-		}
-
-	}
-
-	return (  
-		<main className="board">
-			<h1>Ta te ti</h1>
-			<button onClick={resetGame}> Reset Game </button>
-			<section className="game">
-				{
-					board.map( (square, index) => {
-						return (
-							<Square
-							key={index}
-							index={index}
-							updateBoard={ updateBoard }
-							>
-							{square}
-							</Square>
-						)
-					} )
+  return (
+    <main className='board'>
+      <h1>Ta te ti</h1>
+      <button onClick={resetGame}> Reset Game </button>
+      <section className='game'>
+        {
+		board.map((square, index) => {
+		 	return (
+  <Square
+    key={index}
+    index={index}
+    updateBoard={updateBoard}
+  >
+    {square}
+  </Square>
+					  )
+		})
 				}
-			</section>
+      </section>
 
-			<section className="turn">
-				<Square isSelected={ turn === TURNS.X }>{ TURNS.X }</Square>
-				<Square isSelected={ turn=== TURNS.O }>{ TURNS.O }</Square>
-			</section>
-			<WinnerModal
-			resetGame={ resetGame }
-			winner={ winner }/>
-		</main>
-
-)
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+      <WinnerModal
+        resetGame={resetGame}
+        winner={winner}
+      />
+    </main>
+  )
 }
 
 export default App
