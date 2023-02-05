36554cf63ef7c861536a67387e1ba3910bc46580
ElsesserMatias <elsesser3@gmail.com>
2023-02-05 01:36:41 -0300

    estilizando

--- projects/04-prueba-react/src/App.jsx
+++ projects/04-prueba-react/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
@@ -8,29 +9,36 @@ export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
+  // efecto para recuperar la cita al cargar la pagina
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const threeFirstWords = fact.split(' ', 3).join(' ') // primeras 3 palabras separadas por espacio
-
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-          })
       })
   }, [])
+
+  // recuperar la imagen cada vez que hay una cita nueva
+  useEffect(() => {
+    if (!fact) return
+    const threeFirstWords = fact.split(' ', 3).join(' ') // primeras 3 palabras separadas por espacio
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  })
 
   return (
     <main>
       <h2> App de gatitos!</h2>
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+      </section>
     </main>
   )
 }
