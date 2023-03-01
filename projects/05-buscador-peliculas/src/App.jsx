b15cc9a9b4cf2c91b268b10f39dd7ce134503c14
ElsesserMatias <elsesser3@gmail.com>
2023-03-01 13:21:16 -0300

    creando referencia del input

--- projects/05-buscador-peliculas/src/App.jsx
+++ projects/05-buscador-peliculas/src/App.jsx
@@ -1,17 +1,34 @@
 import './App.css'
+import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
+// useRef es un hook de React que nos permite crear una referencia mutable a un elemento del DOM o a un valor cualquiera que persiste entre renderizados
+
 function App () {
   const { movies } = useMovies()
+  const inputRef = useRef() // pasamos la constante como propiedad "ref" en el input
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    // // current es una propiedad nativa de react, se utiliza para acceder al valor actual del elemento
+    // const value = inputRef.current.value
+    // console.log(value)
+
+    const { query } = Object.fromEntries(
+      new window.FormData(event.target)
+    )
+    console.log(query)
+  }
 
   return (
     <div className='page'>
 
       <header>
         <h1>Buscador de peliculas</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix ...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input name='query' ref={inputRef} placeholder='Avengers, Star Wars, The Matrix ...' />
           <button type='submit'> Buscar</button>
         </form>
       </header>
