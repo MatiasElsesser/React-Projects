8138f73104113c6133a825b40660bdf876085463
ElsesserMatias <elsesser3@gmail.com>
2023-03-06 22:07:20 -0300

    Inicio ShopCart

---
+++ projects/06-carro-compras/src/App.jsx
@@ -0,0 +1,10 @@
+import { Products } from '../components/Products'
+import { products } from '../mocks/products.json'
+
+function App () {
+  return (
+    <Products products={products} />
+  )
+}
+
+export default App
