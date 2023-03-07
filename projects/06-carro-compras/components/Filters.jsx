3c2a54e09b8493dbb8d88b423186a822bda58add
ElsesserMatias <elsesser3@gmail.com>
2023-03-07 14:19:31 -0300

    Aplicando funcionamiento de filtros

---
+++ projects/06-carro-compras/components/Filters.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './Filters.css'
+
+export function Filters ({ onChange }) {
+  const [minPrice, setMinPrice] = useState(0)
+
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value)
+    onChange(prevState => ({
+      ...prevState,
+      minPrice: event.target.value
+    }))
+  }
+
+  const handleChangeCategory = (event) => {
+    onChange(prevState => ({
+      ...prevState,
+      category: event.target.value
+    }))
+  }
+
+  return (
+    <section className='filters'>
+
+      <div>
+        <label htmlFor='price'>Precio a partir de: </label>
+        <input
+          type='range'
+          id='price'
+          min='0'
+          max='1000'
+          onChange={handleChangeMinPrice}
+        />
+        <span>${minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor='category'>Categoria</label>
+        <select id='category' onChange={handleChangeCategory}>
+          <option value='all'>Todas</option>
+          <option value='laptops'>Portatiles</option>
+          <option value='smartphones'>Celulares</option>
+        </select>
+      </div>
+
+    </section>
+  )
+}
--- projects/06-carro-compras/src/App.jsx
+++ projects/06-carro-compras/src/App.jsx
@@ -1,9 +1,33 @@
 import { Products } from '../components/Products'
-import { products } from '../mocks/products.json'
+import { products as initialProducts } from '../mocks/products.json'
+import { useState } from 'react'
+import { Header } from '../components/Header'
 
 function App () {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
+  const filteredProducts = filterProducts(products)
+
   return (
-    <Products products={products} />
+    <>
+      <Header changeFilters={setFilters} />
+      <Products products={filteredProducts} />
+    </>
   )
 }
 
