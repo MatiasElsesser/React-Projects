e1f832cfa3617df6674d6e28dc5606bdfc6a79d5
ElsesserMatias <elsesser3@gmail.com>
2023-03-15 13:32:06 -0300

    Fix search

--- projects/01-pokedex/src/components/HeaderApp.jsx
+++ projects/01-pokedex/src/components/HeaderApp.jsx
@@ -1,21 +1,39 @@
-import { useId, useState, useRef } from 'react'
-import { useSearchPokemons } from '../services/searchPokemons'
-// import { Pokecard } from './Pokecard'
+import { useId, useState, useRef, useEffect } from 'react'
+// import { useSearchPokemons } from '../services/searchPokemons'
+import { Pokecard } from './Pokecard'
 import './HeaderApp.css'
 
 export const HeaderApp = () => {
   const inputSearchPokemon = useId()
   const inputRef = useRef()
   const [search, setSearch] = useState('')
-  const { pokemon } = useSearchPokemons(search)
+  // const { pokemon } = useSearchPokemons(search)
+  const [pokemonSearch, setPokemonSearch] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setSearch('')
     // console.log(inputRef.current.value)
     const value = inputRef.current.value
     setSearch(value)
-    console.log(pokemon)
   }
+
+  useEffect(() => {
+    if (pokemonSearch.includes(inputRef.current.value)) return
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+      .then(res => res.json())
+      .then(json => {
+        const newPoke = {
+          name: json.name,
+          id: json.id,
+          img: json.sprites.front_default,
+          types: json.types,
+          stats: json.stats
+        }
+        setPokemonSearch(poke => [...poke, newPoke])
+      })
+  }, [search])
 
   return (
     <header className='header'>
@@ -27,15 +45,21 @@ export const HeaderApp = () => {
       </form>
       <div className='header-results'>
         <p>Resultados:</p>
-        {/* {search &&
-          <Pokecard
-            key={search.id}
-            img={search.img}
-            name={search.name}
-            id={search.id}
-            types={search.types}
-            stats={search.stats}
-          />} */}
+        <div className='results-container'>
+          {pokemonSearch.length > 0 &&
+        pokemonSearch.map((el) => {
+          return (
+            <Pokecard
+              key={el.id}
+              img={el.img}
+              name={el.name}
+              id={el.id}
+              types={el.types}
+              stats={el.stats}
+            />
+          )
+        })}
+        </div>
       </div>
     </header>
   )
--- projects/01-pokedex/src/services/searchPokemons.js
+++ projects/01-pokedex/src/services/searchPokemons.js
@@ -1,24 +1,24 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 
-export const useSearchPokemons = (search) => {
-  const [pokemon, setPokemon] = useState({})
+// export const useSearchPokemons = (search) => {
+//   const [pokemon, setPokemon] = useState({})
 
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`)
-      .then(res => res.json())
-      .then(json => {
-        const pokemon = {
-          name: json.name,
-          id: json.id,
-          img: json.sprites.front_default,
-          types: json.types,
-          stats: json.stats
-        }
-        setPokemon({ pokemon })
-      })
-  }, [search])
-  return pokemon
-}
+//   useEffect(() => {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`)
+//       .then(res => res.json())
+//       .then(json => {
+//         const pokemon = {
+//           name: json.name,
+//           id: json.id,
+//           img: json.sprites.front_default,
+//           types: json.types,
+//           stats: json.stats
+//         }
+//         setPokemon({ pokemon })
+//       })
+//   }, [search])
+//   return pokemon
+// }
 
 // export const searchPokemons = async (search) => {
 //   try {
