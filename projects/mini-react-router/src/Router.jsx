0a4cbde853df356a4637edcb0a2b24e8284326a7
ElsesserMatias <elsesser3@gmail.com>
2023-04-22 19:17:48 -0300

    fix bugs

--- projects/mini-react-router/src/Router.jsx
+++ projects/mini-react-router/src/Router.jsx
@@ -30,7 +30,7 @@ export function Router ({ children, routes = [], defaulltComponent: DefaultCompo
 
     return isRoute ? props : null
   })
-  const routesToUse = routes.concat(routesFromChildren)
+  const routesToUse = routes.concat(routesFromChildren).filter(Boolean)
 
   const Page = routesToUse.find(({ path }) => {
     if (path === currentPath) return true
