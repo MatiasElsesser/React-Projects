5034f83768f4df78c12c1998742c032976d22e98
ElsesserMatias <elsesser3@gmail.com>
2023-05-04 13:39:34 -0300

    create reducer

--- projects/Clon-google-translate/src/App.tsx
+++ projects/Clon-google-translate/src/App.tsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useReducer } from 'react'
 
 const initialState = {
   fromLanguage: 'auto',
@@ -10,10 +11,52 @@ const initialState = {
 }
 
 function reducer (state = initialState, action) {
+  const { type, payload } = action
 
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      result: '',
+      fromText: payload
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: payload
+    }
+  }
+
+  return state
 }
 
 function App () {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
       <h1> Translate </h1>
