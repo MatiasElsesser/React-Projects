d785ef05f78660e30cadffcbb4e460d28c7fd3d9
ElsesserMatias <elsesser3@gmail.com>
2023-09-13 22:53:50 -0300

    finish project

--- projects/crud-react-redux/src/App.tsx
+++ projects/crud-react-redux/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { CreateNewUser } from './components/CreateNewUser'
 import { ListOfUsers } from './components/ListOfUsers'
-
+import { Toaster } from 'sonner'
 function App () {
   return (
     <>
       <h1>Proyecto CRUD con React y Redux</h1>
       <ListOfUsers />
       <CreateNewUser />
+      <Toaster richColors />
     </>
   )
 }
--- projects/crud-react-redux/src/store/index.ts
+++ projects/crud-react-redux/src/store/index.ts
@@ -1,9 +1,52 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, Middleware } from '@reduxjs/toolkit'
 import usersReducer from './users/slice'
+import { toast } from 'sonner'
 
-const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
+const persistanceLocalStorageMiddleware: Middleware = (store) => (next) => (action) => {
   next(action)
   localStorage.setItem('__redux_state__', JSON.stringify(store.getState()))
+}
+
+const syncWithDatabase: Middleware = (store) => (next) => (action) => {
+  const { type, payload } = action
+  console.log(store)
+  next(action)
+
+  if (type === 'users/deleteUsersById') {
+    fetch(`https://jsonplaceholder.typicode.com/users/${payload}`, {
+      method: 'DELETE'
+    })
+      .then(res => {
+        if (res.ok) {
+          console.log(res)
+          toast.success(`Usuario ${payload} eliminado correctamente`)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  if (type === 'users/addNewUser') {
+    fetch(`https://jsonplaceholder.typicode.com/users/${payload}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        // pendiente (recuperar datos del formulario y llenar el cuerpo de la peticion con id, title y body)
+        title: `${payload.name}`,
+        body: `${payload.email}`,
+        userId: `${payload}`
+      }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
+    })
+      .then(res => {
+        if (res.ok) {
+          console.log(res)
+          toast.success(`Usuario ${payload.name} creado correctamente`)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 }
 
 // store es el sitio donde vamos a guardar nuestro estado
@@ -13,7 +56,7 @@ export const store = configureStore({
     // le decimos que una parte de los reducer viene del slice user
     users: usersReducer
   },
-  middleware: [persistanceLocalStorageMiddleware]
+  middleware: [persistanceLocalStorageMiddleware, syncWithDatabase]
 })
 // le decimos que de la funcion getState de store, el tipo que devuelva va a ser el tipo que tenga RootState
 export type RootState = ReturnType<typeof store.getState>
